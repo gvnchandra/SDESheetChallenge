72f50e7457f86b0303c1cec2a4659acab8b6e24a
gvnchandra <gvnchandra1215@gmail.com>
2022-06-12 21:16:20 +0530

    Complete Longest_Consecutive_Sequence.java

---
+++ Day4/src/Longest_Consecutive_Sequence.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Longest_Consecutive_Sequence {
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+
+
+        Set<Integer> has= new HashSet<>();
+        for (int i=0;i<n;i++){
+            has.add(a[i]);
+        }
+
+        int maxCount=0;
+        for(int val:a){
+            if(!has.contains(val-1)){
+                int curr=1, currNum=val+1;
+                while(has.contains(currNum)) {
+                    curr++;
+                    currNum++;
+                }
+                maxCount=Math.max(maxCount, curr);
+            }
+        }
+
+        System.out.println("Longest Consecutive Sequence is "+maxCount);
+    }
+}
